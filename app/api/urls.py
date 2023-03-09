2de0a19d8cb21ef55616911935639db52dc3dddb
Hyoungsooo <aaa57403@gmail.com>
2023-03-09 09:36:11 +0900

    update

--- app/api/urls.py
+++ app/api/urls.py
@@ -19,7 +19,6 @@ import random
 from django.db.models import Q, Count
 
 
-
 api = NinjaAPI()
 
 
@@ -153,7 +152,6 @@ def stockfish_battle(request, level: int, depth: int, fen: str):
     stockfish.set_skill_level(level)
     stockfish.set_depth(depth)
 
-<<<<<<< HEAD
     stockfish = is_fen_valid(stockfish, fen)
     if stockfish:
 
@@ -255,12 +253,6 @@ def get_other_related_opening(request, opening: str):
         'name', 'fen', 'uci').distinct('name'))
 
     return JsonResponse(data, safe=False)
-=======
-    if stockfish.is_fen_valid(fen=fen):
-        stockfish.set_fen_position(fen)
-
-    return stockfish.get_best_move_time(1000)
->>>>>>> b2630fe1222f24464e465bd17e382232432cb563
 
 
 urlpatterns = [
